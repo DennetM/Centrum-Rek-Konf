8bb8d7487e5c695bb9d6b233472976f59f4bd5c2
DennetM <DennetM.alt@gmail.com>
2013-12-07 18:14:51 +0100

    Revert "Revert "Database""
    This reverts commit 5e2f420e235b205eaafe8a235971c30ec17374f5.

---
+++ database/IO-database/POJO/Guest.java
@@ -0,0 +1,64 @@
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "Guest")
+public class Guest {
+
+	@DatabaseField(generatedId = true)
+    private int GuestId;
+    @DatabaseField
+    private String Name;
+    @DatabaseField
+    private String Surname;
+    @DatabaseField
+    private String TelephoneNumber;
+    @DatabaseField
+    private String Email;
+
+    public Guest(){
+    	
+    }
+    
+    public Guest(int GuestId, String Name, String Surname, String TelephoneNumber, String Email){
+    	this.GuestId = GuestId;
+    	this.Name = Name;
+    	this.Surname = Surname;
+    	this.TelephoneNumber = TelephoneNumber;
+    	this.Email = Email;
+    }
+    
+    public int getGuestId() {
+        return GuestId;
+    }
+    public void setGuestId(int GuestId) {
+        this.GuestId = GuestId;
+    }
+    
+    public String getName() {
+        return Name;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+    public String getSurname() {
+        return Surname;
+    }
+    public void setSurname(String Surname) {
+        this.Surname = Surname;
+    }
+    
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+    
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+}
